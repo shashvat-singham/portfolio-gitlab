3257a5f4bd1acfc22212d35170f60f41fa66e2b1
shashvat singham <78735607+shashvat-singham@users.noreply.github.com>
2023-09-07 17:35:04 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -362,9 +362,9 @@ const podcastSection = {
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
-    "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+    "",
+  number: "",
+  email_address: ""
 };
 
 // Twitter Section
