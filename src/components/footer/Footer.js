57e1bf218ae239f2e5e8e4d6dc072aa0221d7bfd
shashvat singham <78735607+shashvat-singham@users.noreply.github.com>
2023-09-07 16:54:12 +0530

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,14 +10,14 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
+          {emoji("Made by Shashvat Singham")}
         </p>
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
-          </a>
-        </p>
+        // <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+        //   Theme by{" "}
+        //   <a href="https://github.com/saadpasta/developerFolio">
+        //     developerFolio
+        //   </a>
+        // </p>
       </div>
     </Fade>
   );
