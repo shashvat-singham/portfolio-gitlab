7a7feaecb2772baf2539ff678bf499383a59ffe8
shashvat singham <78735607+shashvat-singham@users.noreply.github.com>
2023-09-10 16:35:48 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -378,7 +378,7 @@ const podcastSection = {
 };
 
 const contactInfo = {
-  title: emoji("Contact Me ☎️"),
+  title: emoji("Reach Out to me!"),
  subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
   // email_address: "Open for opportunities: No"
