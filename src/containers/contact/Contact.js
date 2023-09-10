21619327582574949cb38088073b11f8d9e1a304
shashvat singham <78735607+shashvat-singham@users.noreply.github.com>
2023-09-10 17:19:46 +0530

    Update Contact.js

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -58,7 +58,7 @@ export default function Contact() {
             ) : (
               <img
                 alt="Man working"
-                src={require("../../assets/images/photo.jpeg")}
+                src={require("../../assets/images/contactMailDark.svg")}
               ></img>
             )}
           </div>
