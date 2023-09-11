dd0dc60ae0822abeeb01dd8c59f850e72b7eb8a7
shashvat singham <shashvat.singham@gmail.com>
2023-09-11 23:11:49 +0000

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -310,8 +310,7 @@ const achievementSection = {
       subtitle: "Position of Responsibilities and academic ",
       image: require("./assets/images/pwaLogo.webp"),
       imageAlt: "Miscellaneous",
-      descBullets: ["All India Rank 223, JEE Mains (B.Arch / B. Planning)","All India Rank 254, National Mathematics Olympiad
-(NMO)"
+      descBullets: ["All India Rank 223, JEE Mains (B.Arch / B. Planning)","All India Rank 254, National Mathematics Olympiad(NMO)"
       ]
       // footerLink: [
       //   {name: "Certification", url: ""},
